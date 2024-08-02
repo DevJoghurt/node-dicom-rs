f7d00009753f21232215ac94ed65e0a639e3cd0a
DevJoghurt <johannes@disedia.com>
2024-08-02 10:57:21 +0200

    added working test

---
+++ __test__/index.spec.ts
@@ -0,0 +1,13 @@
+import test from 'ava'
+
+import { DicomFile } from '../index'
+
+test('read dicom file', (t) => {
+  const file = new DicomFile();
+
+  file.open('./__test__/fixtures/test.dcm');
+
+  const data = file.getElements();
+
+  t.is(data.patientName, 'node-metrics');
+})
